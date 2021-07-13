8e9a028456081f23935b323c96ea61582fd6ba8c
ppumang <minj219aa@gmail.com>
2021-07-13 15:06:27 +0900

    hi

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 })
 
+app.get("/hi", (req,res) => {
+    res.sendFile(path.join(__dirname, "hi.html"));
+})
+
 app.listen(3001, (req,res) => {
     console.log("listening on port 3001")
 })
