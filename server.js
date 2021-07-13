8ce3567afa49899dd01b039cff3768d14aa041b4
Ed <ed@Eds-MacBook-Pro.local>
2021-07-13 11:43:04 +0900

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const fs = require("fs");
+
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+})
+
+app.listen(3001, (req,res) => {
+    console.log("listening on port 3001")
+})
